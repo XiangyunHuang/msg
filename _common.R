6fbef9f8fe54008cdb09063c185b8026d2b7939d
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-08-16 09:29:50 +0800

    引入 magick 处理图片格式转化问题

---
+++ _common.R
@@ -0,0 +1,6 @@
+# convert pdf to png
+to_png <- function(fig_path) {
+  png_path <- sub("\\.pdf$", ".png", fig_path)
+  magick::image_write(magick::image_read_pdf(fig_path), format = "png", path = png_path)
+  return(png_path)
+}
