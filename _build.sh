1ea6457119c6542b5e97c2bf6eb44d9c1804d7b1
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-08-18 18:08:01 +0800

    校对系统、历史和元素三章

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Rscript --no-save -e "bookdown::render_book(input = 'index.Rmd', output_dir = '_book', config_file = '_bookdown.yml')"
+xvfb-run Rscript --no-save -e "bookdown::render_book(input = 'index.Rmd', output_dir = '_book', config_file = '_bookdown.yml')"
