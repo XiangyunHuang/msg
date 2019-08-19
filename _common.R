1a7de3e1b593dad163e9488b12eaec5ca20533e4
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-08-19 11:44:26 +0800

    * rgl 动态图形单独窗口显示，点击静态图即可弹出新窗口
    * 能够编译 PDF，本地测试通过，但是我们还是不会制作放出 PDF 除非这本书准备出版

--- _common.R
+++ _common.R
@@ -4,3 +4,6 @@ to_png <- function(fig_path) {
   magick::image_write(magick::image_read_pdf(fig_path), format = "png", path = png_path)
   return(png_path)
 }
+
+is_latex <- identical(knitr::opts_knit$get("rmarkdown.pandoc.to"), "latex")
+is_html <- identical(knitr::opts_knit$get("rmarkdown.pandoc.to"), "html")
