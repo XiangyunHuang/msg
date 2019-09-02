faeea66bfc17df9a166c01599a0d3690206078bf
Xiangyun Huang <xiangyunfaith@outlook.com>
2019-09-02 18:34:36 +0800

    添加 xvfb-run 运行 Rscript

--- _build.sh
+++ _build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-Rscript --no-save -e "bookdown::render_book(input = 'index.Rmd', output_format = 'all', output_dir = '_book', config_file = '_bookdown.yml')"
+xvfb-run Rscript --no-save -e "bookdown::render_book(input = 'index.Rmd', output_format = 'all', output_dir = '_book', config_file = '_bookdown.yml')"
